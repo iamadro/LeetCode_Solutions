a7786b8a776ce49c71acf9287a9de24dd4d0b4bb
Aditya Routh <113755809+iamadro@users.noreply.github.com>
2025-08-01 15:53:20 +0530

    Time: 663 ms (21.48%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,15 +1,16 @@
 SELECT
     items.product_id,
-    (CASE WHEN p2.dated IS NULL THEN 10 ELSE (
-        SELECT new_price from products where product_id = items.product_id and change_date = p2.dated
-    ) END) price
-FROM (
-    SELECT DISTINCT product_id from products
-) items
-LEFT JOIN (
-    SELECT product_id, max(change_date) as dated
-    FROM products
-    where DATEDIFF('2019-08-16', change_date) >= 0
-    GROUP BY product_id
-) p2 ON items.product_id = p2.product_id
-;
+    COALESCE(p.new_price, 10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM products) AS items
+LEFT JOIN 
+    (
+        SELECT product_id, MAX(change_date) AS max_date
+        FROM products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    ) AS last_change 
+    ON items.product_id = last_change.product_id
+LEFT JOIN 
+    products AS p 
+    ON last_change.product_id = p.product_id AND last_change.max_date = p.change_date;
