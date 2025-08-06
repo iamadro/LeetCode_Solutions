fb202d715bb4d48ae7f8d390907efe53dc22b205
Aditya Routh <113755809+iamadro@users.noreply.github.com>
2025-08-06 10:50:05 +0530

    Time: 721 ms (89.49%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,19 @@
+SELECT 
+    q2.person_name
+    -- q.cumm_weight
+FROM
+    Queue q2
+JOIN (
+    SELECT
+        person_id,
+        (
+            SUM(weight) OVER(ORDER BY turn) 
+        ) cumm_weight
+    FROM
+        Queue
+) q
+ON q.person_id = q2.person_id
+WHERE cumm_weight <= 1000
+ORDER BY cumm_weight DESC
+LIMIT 1
+;
