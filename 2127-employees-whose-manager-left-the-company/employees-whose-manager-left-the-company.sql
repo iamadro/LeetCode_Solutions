23efc68151c2fa4b02240ae34b7cb641acb49057
Aditya Routh <113755809+iamadro@users.noreply.github.com>
2025-08-06 12:43:41 +0530

    Time: 350 ms (38.76%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,25 +1,25 @@
--- SELECT 
---     e.employee_id
--- FROM Employees e
--- LEFT JOIN Employees m
--- ON m.employee_id = e.manager_id
--- WHERE e.manager_id IS NOT NULL AND m.employee_id IS NULL AND e.salary < 30000
--- ORDER BY e.employee_id
--- ;
-
 SELECT 
     e.employee_id
 FROM Employees e
-WHERE 
-    e.manager_id IS NOT NULL
-    AND
-    NOT EXISTS (
-        SELECT m.employee_id 
-        FROM Employees m
-        WHERE m.employee_id = e.manager_id
-    )
-    AND 
-    e.salary < 30000
-ORDER BY 
-    e.employee_id
+LEFT JOIN Employees m
+ON m.employee_id = e.manager_id
+WHERE e.manager_id IS NOT NULL AND m.employee_id IS NULL AND e.salary < 30000
+ORDER BY e.employee_id
 ;
+
+-- SELECT 
+--     e.employee_id
+-- FROM Employees e
+-- WHERE 
+--     e.manager_id IS NOT NULL
+--     AND
+--     NOT EXISTS (
+--         SELECT m.employee_id 
+--         FROM Employees m
+--         WHERE m.employee_id = e.manager_id
+--     )
+--     AND 
+--     e.salary < 30000
+-- ORDER BY 
+--     e.employee_id
+-- ;
