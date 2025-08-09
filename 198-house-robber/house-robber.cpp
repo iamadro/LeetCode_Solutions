9247293ebcc631899c45aa89100009b2b0885902
Aditya Routh <113755809+iamadro@users.noreply.github.com>
2025-08-09 17:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.50%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -20,7 +20,20 @@ private:
 
 public:
     int rob(vector<int>& nums) {
-        memo.resize(nums.size(), -1);
-        return steal(nums, 0, nums.size() - 1);
+        // memo.resize(nums.size(), -1);
+        // return steal(nums, 0, nums.size() - 1);
+
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n);
+        dp[n-1] = nums[n-1];                    // 2
+        dp[n-2] = max(nums[n-1], nums[n-2]);    // 2
+
+        for (int i = n - 3; i > -1; i--) {
+            dp[i] = max(dp[i+1], nums[i] + dp[i+2]);
+        }
+
+        return dp[0];
     }
 };
